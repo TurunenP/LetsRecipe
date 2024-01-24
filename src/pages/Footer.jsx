38e71bb8e7604a9ca62f8f76af3d552fba2d8795
Purity Turunen <k7jepu00@students.oamk.fi>
2024-01-24 10:57:02 +0200

    Initial commit

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,15 @@
+// Footer.jsx
+
+import React from 'react';
+import '../App.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      {/*Other footer content goes here */}
+      <p>&copy; 2023. Food Recipes. All rights reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
